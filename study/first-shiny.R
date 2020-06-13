10d92975e62891c3a1c2faaddedd59de6d4802ab
SeoyeonH <59638493+SeoyeonH@users.noreply.github.com>
2020-06-13 11:23:06 +0900

    first-shiny

---
+++ study/first-shiny.R
@@ -0,0 +1,11 @@
+
+#----------shiny app start
+
+# install.packages('shiny')
+library(shiny)
+
+runExample("01_hello")
+# "01_hello", "02_text", "03_reactivity", "04_mpg", "05_sliders", 
+# "06_tabsets", "07_widgets", "08_html", "09_upload", "10_download", "11_timer"
+
+runExample("04_mpg")
